8c6eae87e3863bf5c633ff897573e8a024337d92
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-05-11 14:39:00 +0000

    automatically backed up by learn

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Tweet from './Tweet';
 
@@ -11,8 +12,20 @@ class TweetWall extends React.Component {
   }
 
   // TODO: componentWillMount()
+  componentWillMount = () => {
+    this.state.tweets = this.props.newTweets;
+  }
   // TODO: shouldComponentUpdate()
+  shouldComponentUpdate = (nextProps, nextState) => {
+    return nextProps.length > 0
+  }
+
   // TODO: componentWillReceiveProps()
+  componentWillReceiveProps = (nextProps) => {
+    this.setState({
+      tweets: [ ...nextProps.newTweets, ...this.state.tweets]
+    })
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
